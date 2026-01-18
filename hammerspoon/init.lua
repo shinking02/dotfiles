78573d711168b95cdd417f1065b46466073d5a45
shinking02 <k.shin0102@gmail.com>
2026-01-18 16:26:16 +0900

    fix: adjust scroll multiplier for mouse scrolling

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -24,17 +24,16 @@ end)
 
 
 local oldmousepos = {}
-local scrollmult = -3 -- negative multiplier makes mouse work like traditional scrollwheel
+local scrollmult = -4 -- negative multiplier makes mouse work like traditional scrollwheel
 dragRightToScroll = hs.eventtap.new({ hs.eventtap.event.types.rightMouseDragged }, function(e)
-    -- print("scroll");
 
     deferred = false
 
     oldmousepos = hs.mouse.getAbsolutePosition()    
 
-    local dx = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
-    local dy = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
-    local scroll = hs.eventtap.event.newScrollEvent({-dx * scrollmult, -dy * scrollmult},{},'pixel')
+    local dx = e:getProperty(hs.eventtap.event.properties["mouseEventDeltaX"])
+    local dy = e:getProperty(hs.eventtap.event.properties["mouseEventDeltaY"])
+    local scroll = hs.eventtap.event.newScrollEvent({dx * scrollmult, dy * scrollmult},{},"pixel")
 
     -- put the mouse back
     hs.mouse.setAbsolutePosition(oldmousepos)
